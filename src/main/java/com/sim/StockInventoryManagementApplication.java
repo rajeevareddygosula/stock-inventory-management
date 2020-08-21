34a98a6a3e57230c2e080f16224ea0db8d2b95f1
716583 <arasavalli.strong@cognizant.com>
2020-08-21 21:48:23 +0530

    updated

--- src/main/java/com/sim/StockInventoryManagementApplication.java
+++ src/main/java/com/sim/StockInventoryManagementApplication.java
@@ -25,22 +25,21 @@ class DemoCommandLineRunner implements CommandLineRunner {
 	@Autowired
 	private StockRepository stockRepository;
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void run(String... args) throws Exception {
 		Stock stock1 = new Stock();
 		stock1.setNumber(UUID.randomUUID());
 		stock1.setName("stock1");
 		stock1.setPrice(10);
-		stock1.setDate(new Date("2020-08-21"));
+		stock1.setDate(new Date());
 		stock1.setQuantity(String.valueOf(QuantityStatus.AVAILABLE));
 		stockRepository.save(stock1);
 		Stock stock2 = new Stock();
 		stock2.setNumber(UUID.randomUUID());
 		stock2.setName("stock2");
 		stock2.setPrice(20);
-		stock2.setDate(new Date("2020-08-21"));
+		stock2.setDate(new Date());
 		stock2.setQuantity(String.valueOf(QuantityStatus.AVAILABLE));
-		stockRepository.save(stock1);
+		stockRepository.save(stock2);
 	}
 }
